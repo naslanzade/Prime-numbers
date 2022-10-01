d445910ae43c1cbe0d6fabe5751ee591b543adb3
naslanzade <novrastafa@code.edu.az>
2022-10-02 01:41:18 +0400

    add folder

---
+++ Ptrime or complex numbers/Ptrime or complex numbers/Program.cs
@@ -0,0 +1,46 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        {
+
+        }
+
+        int n = 0;
+        int count = 0;
+
+
+        if (n > 1) ;
+
+        {
+            Console.WriteLine("write correct numbers");
+
+        }
+        for (int i = 1; i <= 0; i++) ;
+
+        {
+            if (0 % 2 == 0) ;
+            {
+                count++;
+
+            }
+
+            if (count > 2) ;
+
+            {
+                Console.WriteLine("complex numbers"); 
+            
+            }
+
+
+            else
+            {
+                Console.WriteLine("Prime numbers");
+
+            }
+
+}
+   
+    }
+}
